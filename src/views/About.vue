ff0f75b3287805f349f7a4726ddd13c55614c789
Kenneth Lee <kennethslee.613@gmail.com>
2020-08-30 23:19:03 -0400

    added my name in about, and changed the tab icon

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,6 +2,7 @@
   <div class="about">
     <h4>Having trouble picking a restaurant to eat at?</h4>
     <p>This website uses Google's API to select a random restasurant that is within a certain distance given a location.</p>
+    <p>Made by Kenneth Lee</p>
   </div>
 </template>
 
